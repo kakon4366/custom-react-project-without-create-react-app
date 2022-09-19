16960ec52cdb56f1a25727c4507f3437d97d7a5e
kakon4366 <kakonbarman4366@gmail.com>
2022-09-19 16:38:56 +0600

    Custom React Project Create without create-react-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app.scss";
+
+const App = () => {
+	return (
+		<h1>
+			Welcome to React App thats build using Webpack and Babel separately
+		</h1>
+	);
+};
+
+export default App;
